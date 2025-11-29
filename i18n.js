e94bbd67500b54d5121e000e51d0e2276397eae6
grrurus-byte <grrurus@gmail.com>
2025-11-29 12:20:02 +0100

    Delete i18n.js

--- i18n.js
+++
@@ -1,8 +0,0 @@
-const translations = {
-    en: { start: "Start Test", welcome: "Welcome to IQ Pro Test" },
-    uk: { start: "Почати Тест", welcome: "Ласкаво просимо до IQ Тесту" }
-};
-
-function t(lang, key) {
-    return translations[lang]?.[key] || translations["en"][key];
-}
